36228924a399f55d6cce146312e5a9e6fe3cf492
Romain Léger <legerrom@gmail.com>
2024-12-01 22:48:23 +0100

    .

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="text-xl font-semibold leading-tight text-gray-800">
-      {{ __('Entrées') }}
+      {{ __('Entrées') }} ({{ $entries->count() }})
     </h2>
   </x-slot>
 
