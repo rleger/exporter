1f361275f8d111e307aae50fa9d6c79616128e89
Romain Léger <legerrom@gmail.com>
2024-12-02 11:05:33 +0100

    only of the current user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,23 +23,30 @@ class DashboardController extends Controller
 
     public function index()
     {
+        $user = auth()->user();
+
         // Récupérer les Calendars avec le nom de l'utilisateur et le nombre d'Entries
         $calendars = Calendar::with('user')
             ->withCount('entries')
             ->get();
 
-        // Récupérer les 20 derniers rendez-vous ajoutés
+        // Récupérer les 20 derniers rendez-vous ajoutés pour l'utilisateur connecté
         $recentAppointments = Appointment::with('entry')
+            ->whereHas('entry', function ($query) use ($user) {
+                $query->where('calendar_id', $user->calendars->pluck('id'));
+            })
             ->orderBy('created_at', 'desc')
-            ->take(10)
+            ->take(20)
             ->get();
 
-        // Récupérer les 20 derniers rendez-vous modifiés
-
+        // Récupérer les 20 derniers rendez-vous modifiés pour l'utilisateur connecté
         $updatedAppointments = Appointment::with('entry')
+            ->whereHas('entry', function ($query) use ($user) {
+                $query->where('calendar_id', $user->calendars->pluck('id'));
+            })
             ->whereColumn('created_at', '!=', 'updated_at')
             ->orderBy('updated_at', 'desc')
-            ->take(10)
+            ->take(20)
             ->get();
 
         return view('dashboard', compact('calendars', 'recentAppointments', 'updatedAppointments'));
