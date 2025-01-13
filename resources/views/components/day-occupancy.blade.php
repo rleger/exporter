efe0456a05e1fbc4c41f48d8a764769a29bcad2e
Romain Léger <legerrom@gmail.com>
2025-01-13 14:57:34 +0100

    .

--- resources/views/components/day-occupancy.blade.php
+++ resources/views/components/day-occupancy.blade.php
@@ -30,13 +30,25 @@ $percentage = round(($hours / 9) * 100);
 $percentage = min($percentage, 100); // Limiter à 100%
 
 // Couleur du badge selon le pourcentage
-if ($percentage < 25) { $color='red' ; } elseif ($percentage < 70) { $color='orange' ; } else { $color='green' ; } if ($color==='red' ) { $bgClass='bg-red-200' ; $fillClass='fill-red-500' ; } elseif ($color==='orange' ) { $bgClass='bg-orange-200' ; $fillClass='fill-orange-500' ; } else { $bgClass='bg-green-200' ; $fillClass='fill-green-500' ; } @endphp {{-- Hack pour éviter que Tailwind ne purge ces classes dynamiques --}} <div style="display: none;">
-  <span class="bg-red-200 bg-orange-200 bg-green-200 fill-red-500 fill-orange-500 fill-green-500"></span>
+if ($percentage < 25) { $color='red' ; } elseif ($percentage < 70) { $color='orange' ; } else { $color='green' ; } if ($color==='red' ) { $bgClass='bg-red-200' ; $textClass='text-red-800' ; $fillClass='fill-red-500' ; $ringClass="ring-red-200" ;} elseif ($color==='orange' ) { $bgClass='bg-orange-200' ; $textClass='text-orange-800' ; $fillClass='fill-orange-500' ; $ringClass="ring-orange-200" ; } else { $bgClass='bg-green-200' ; $textClass='text-green-800' ; $fillClass='fill-green-500' ; $ringClass="ring-green-200" ; } @endphp {{-- Hack pour éviter que Tailwind ne purge ces classes dynamiques --}} <div style="display: none;">
+
+
+
+
+
+
+
+  <span class="text-red-800 text-orange-800 text-green-800 bg-red-200 bg-orange-200 bg-green-200 ring-red-200 ring-orange-200 ring-green-200 fill-red-500 fill-orange-500 fill-green-500"></span>
+
+
+
+
+
   </div>
 
   <div class="relative inline-flex items-center justify-center gap-x-1.5
-         rounded-full text-xs font-medium text-gray-900
-         ring-1 ring-inset ring-gray-200
+         rounded-full text-xs font-medium {{ $textClass  }}
+         ring-1 ring-inset {{ $ringClass  }}
          overflow-hidden w-32 h-6 px-2">
     <!-- Barre de progression en arrière-plan -->
     <div class="absolute left-0 top-0 h-full {{ $bgClass }}" style="width: {{ $percentage }}%;"></div>
