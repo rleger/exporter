d476db6dd700f84fea0676b9fc992843a59ccf19
Romain Léger <legerrom@gmail.com>
2025-01-14 15:07:39 +0100

    visual tweaks

--- resources/views/components/appointments/entry-name.blade.php
+++ resources/views/components/appointments/entry-name.blade.php
@@ -1,10 +1,11 @@
  @if(str_contains($item->subject, 'Annulé'))
- <a class="inline-block text-blue-800 line-through hover:line-through hover:text-blue-700" href="{{ route('appointments.show', $item->entry->id) }}">
+ {{-- Appointment is canceelled --}}
+ <a class="inline-block font-semibold line-through text-blue-800/70 hover:line-through hover:text-blue-700" href="{{ route('appointments.show', $item->entry->id) }}">
    {{ $item->entry->name }} {{ $item->entry->lastname }}
  </a>
  @else
- <a class="inline-block text-blue-800 hover:text-blue-700 hover:underline" href="{{ route('appointments.show', $item->entry->id) }}">
-
+ {{-- Appointment is not canceelled --}}
+ <a class="inline-block font-semibold text-blue-800 hover:text-blue-700 hover:underline" href="{{ route('appointments.show', $item->entry->id) }}">
    {{ $item->entry->name }} {{ $item->entry->lastname }}
  </a>
  @endif
